523104da8b1f8100cb2ceaa4121152ac96e34286
kotolin0xD2A1 <kotolin0xd2a1@chocomintice.com>
2025-09-04 14:06:47 +0900

    プロジェクト ファイルを追加します。

---
+++ YMM4ScriptK/plugin.fs
@@ -0,0 +1,22 @@
+﻿namespace YMM4ScriptK
+
+open System.Collections.Generic
+open YukkuriMovieMaker.Commons
+open YukkuriMovieMaker.Exo
+open YukkuriMovieMaker.Player.Video
+open YukkuriMovieMaker.Plugin.Effects
+
+[<VideoEffect("YMMScriptK", [|"いろいろ"|], [||])>]
+type internal YMM4ScriptK() =
+    inherit VideoEffectBase()
+
+    override this.Label = "YMM4ScriptK"
+
+    override this.CreateExoVideoFilters(keyFrameIndex: int, exoOutputDescription: ExoOutputDescription) =
+        [] :> IEnumerable<string>
+
+    override this.CreateVideoEffect(devices: IGraphicsDevicesAndContext) =
+        new YMM4ScriptK_Processor(devices, this) :> IVideoEffectProcessor
+
+    override this.GetAnimatables() =
+        ([] : IAnimatable list) :> IEnumerable<IAnimatable>
---
+++ YMM4ScriptK/processor.fs
@@ -0,0 +1,36 @@
+﻿namespace YMM4ScriptK
+
+open System
+open Vortice.Direct2D1
+open YukkuriMovieMaker.Commons
+open YukkuriMovieMaker.Player.Video
+
+type internal YMM4ScriptK_Processor(devices: IGraphicsDevicesAndContext, _owner: obj) =
+
+    let effect = new Vortice.Direct2D1.Effects.Opacity(devices.DeviceContext)
+    let outputImage: ID2D1Image = effect.Output
+
+    member val Output: ID2D1Image = outputImage with get
+
+    member this.SetInput(input: ID2D1Image option) =
+        match input with
+        | Some img -> effect.SetInput(0, img, true)
+        | None -> effect.SetInput(0, null, true)
+
+    member this.ClearInput() =
+        effect.SetInput(0, null, true)
+
+    member this.Update(effectDescription: EffectDescription) =
+        effectDescription.DrawDescription
+
+    interface IVideoEffectProcessor with
+        member this.Output = this.Output
+        member this.SetInput(input) = this.SetInput(Option.ofObj input)
+        member this.ClearInput() = this.ClearInput()
+        member this.Update(effectDescription) = this.Update(effectDescription)
+
+    interface IDisposable with
+        member this.Dispose() =
+            effect.SetInput(0, null, true)
+            this.Output.Dispose()
+            effect.Dispose()
